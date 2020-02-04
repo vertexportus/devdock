4bee673702101b8763ac5e3e2c8135ed37a14e70
Daniel Alves <daniel.alves@portus.tech>
2020-02-04 16:31:28 -0300

    spruced up direnv setup a bit

--- env/generate_envs.sh
+++ env/generate_envs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# versions
+echo -e "\n\e[0;36mdevdock version: \e[1;36m$([[ -d $DEVDOCK_PATH ]] && cat $DEVDOCK_PATH/VERSION || echo devdock not cloned yet)\e[0;33m"
+cmp --silent $PROJECT_PATH/.envrc $DEVDOCK_PATH/env/direnv && echo "" || echo -e "\e[1;33mwarning: \e[0;33mdirenv master file is outdated. please run \e[1;33mdev config direnv\e[0;33m to update\n"
+
 # project/docker
 if [[ -z $ENV ]]; then export ENV=dev; fi
 if [[ -z $PROJECT_NAME ]]; then export PROJECT_NAME=${PROJECT_PATH##*/}; fi
@@ -15,6 +19,10 @@ url=$(python3 $DEVDOCK_PATH/env/get_env_url.py)
 if [[ -n $url ]]; then export BASE_URL=$url; else export BASE_URL=localhost; fi
 
 # php xdebug
-if [[ -z $XDEBUG_ENABLE ]]; then export XDEBUG_ENABLE=0; fi
 is_php_used=$(cat "${PROJECT_PATH}/project.yaml" | grep -Po "template: php")
-if [[ -n $is_php_used ]]; then export XDEBUG_REMOTE_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+'); fi
+if [[ -n $is_php_used ]]; then
+  if [[ -z $XDEBUG_ENABLE ]]; then export XDEBUG_ENABLE=0; fi
+  export XDEBUG_REMOTE_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
+fi
+
+$DEVDOCK_PATH/env/print_envs.sh
---
+++ env/print_envs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+## show information of current env
+echo -e "\e[0;36m Env:         \e[1;36m$ENV"
+echo -e "\e[0;36m Base URL:    \e[1;36m$BASE_URL"
+echo -e "\e[0;33m"
