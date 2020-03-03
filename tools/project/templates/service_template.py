69a4512c93e07cd7449d779488ca703c09a07113
Daniel Alves <daniel.alves@portus.tech>
2020-03-03 11:10:49 -0300

    added support for template inheritance; added build-files generation;

--- tools/project/templates/service_template.py
+++ tools/project/templates/service_template.py
@@ -1,12 +1,14 @@
+from utils import env
 from .container_template import ContainerTemplate
 from utils.yaml_template_object import YamlTemplateObject
+from ..generation import generate_build_files
 
 
 class ServiceTemplate(YamlTemplateObject):
     hidden_fields = ['service']
     yaml_tag = '!ServiceTemplate'
     name: str
-    base_path: str
+    base_path: str or list
     containers: dict
 
     @property
@@ -28,6 +30,10 @@ class ServiceTemplate(YamlTemplateObject):
             template_params=template_params,
             template_name=f"{self.base_path}/config.yaml"
         )
+        inherits = self.try_get('inherits', False)
+        if inherits:
+            self.base_path = [inherits.replace('.', '/'), self.base_path]
+            self.merge_load_template_data(f"{self.base_path[0]}/config.yaml")
         self.containers = {k: ContainerTemplate(k, self, templates, template_params, v)
                            for k, v in self.try_get('containers', {}).items()}
 
@@ -38,3 +44,15 @@ class ServiceTemplate(YamlTemplateObject):
     def generate_compose(self, compose_services, compose_volumes):
         for container in self.containers.values():
             container.generate_compose(compose_services, compose_volumes)
+        self.generate_build_files()
+
+    def generate_build_files(self):
+        template_params = {
+            'defaults': self.service.master.defaults,
+            'project': self.service.project,
+            'service': self.service,
+            'siblings': self.containers
+        }
+        dest_path = f"build/{self.service.master.current_env}"
+        paths = list(map(lambda x: f"{x}/build", self.base_path if type(self.base_path) == list else [self.base_path]))
+        generate_build_files(paths, dest_path, self.templates, **template_params)
