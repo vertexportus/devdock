483ee3f7dd7e75fd4ac240b5cc23d9c2aa4788ac
Daniel Alves <daniel.alves@portus.tech>
2020-03-11 16:37:09 -0300

    cleanup

--- env/utils/php.py
+++
@@ -1,6 +0,0 @@
-from internals.project_data import load_project_and_defaults, filter_services
-from internals.service_version import get_tech_version_from_service
-
-(project, defaults) = load_project_and_defaults()
-print(':'.join(
-    list(map(lambda x: get_tech_version_from_service('php', x, defaults), filter_services(project, 'php')))))
