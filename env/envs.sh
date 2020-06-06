d4f2bf5feb71cc412e64325d3dce5654e6059af7
Daniel Alves <daniel.alves@portus.tech>
2020-06-06 11:28:34 -0300

    small changes

--- env/envs.sh
+++ env/envs.sh
@@ -2,8 +2,13 @@
 # shellcheck disable=SC2086
 # shellcheck disable=SC2155
 
+# colors
+color_text="\e[0;36m"
+color_text_hl="\e[1;36m"
+nc="\e[0;33m"
+
 # versions
-echo -e "\n\e[0;36mdevdock version: \e[1;36m$([[ -d $DEVDOCK_PATH ]] && cat $DEVDOCK_PATH/VERSION || echo devdock not cloned yet)\e[0;33m"
+echo -e "\n${color_text}devdock version: ${color_text_hl}$([[ -d $DEVDOCK_PATH ]] && cat $DEVDOCK_PATH/VERSION || echo devdock not cloned yet)\e[0;33m"
 cmp --silent $PROJECT_PATH/.envrc $DEVDOCK_PATH/env/direnv && echo "" || echo -e "\e[1;33mwarning: \e[0;33mdirenv master file is outdated. please run \e[1;33mdev config env -g\e[0;33m to update\n"
 
 # project/docker
@@ -42,16 +47,16 @@ postgres=$(python3 $devdock_utils/get_tech_version.py postgres)
 minio=$(python3 $devdock_utils/get_tech_version.py minio)
 
 ## show information of current env
-echo -e "\e[0;36m Project   :   \e[1;36m$PROJECT_NAME"
-echo -e "\e[0;36m Env       :   \e[1;36m$ENV"
-echo -e "\e[0;36m Base URL  :   \e[1;36m$BASE_URL"
-if [[ -n $php ]]; then echo -e "\e[0;36m PHP       :   \e[1;36m$php $([ $XDEBUG_ENABLE -eq 0 ] && echo no-xdebug || echo xdebug)"; fi
+echo -e "${color_text} Project   :   ${color_text_hl}$PROJECT_NAME"
+echo -e "${color_text} Env       :   ${color_text_hl}$ENV"
+echo -e "${color_text} Base URL  :   ${color_text_hl}$BASE_URL"
+if [[ -n $php ]]; then echo -e "${color_text} PHP       :   ${color_text_hl}$php $([ $XDEBUG_ENABLE -eq 0 ] && echo no-xdebug || echo xdebug)"; fi
 if [[ -n $node ]]; then
   node_adds=
   if [[ -n $angular ]]; then node_adds="(angular: $angular)"; fi
-  echo -e "\e[0;36m Node      :   \e[1;36m$node $node_adds"
+  echo -e "${color_text} Node      :   ${color_text_hl}$node $node_adds"
 fi
-if [[ -n $dotnetcore ]]; then echo -e "\e[0;36m .net Core :   \e[1;36m$dotnetcore"; fi
-if [[ -n $postgres ]]; then echo -e "\e[0;36m Postgres  :   \e[1;36m$postgres"; fi
-if [[ -n $minio ]]; then echo -e "\e[0;36m Minio(S3) :   \e[1;36m$minio"; fi
-echo -e "\e[0;33m"
+if [[ -n $dotnetcore ]]; then echo -e "${color_text} .net Core :   ${color_text_hl}$dotnetcore"; fi
+if [[ -n $postgres ]]; then echo -e "${color_text} Postgres  :   ${color_text_hl}$postgres"; fi
+if [[ -n $minio ]]; then echo -e "${color_text} Minio(S3) :   ${color_text_hl}$minio"; fi
+echo -e "${nc}"
